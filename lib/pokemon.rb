cc271dbcff12c5e1b2fab905ba197f19e246f78f
Alvin Coulson <aaacoulson@gmail.com>
2020-04-07 02:14:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,14 +17,18 @@ class Pokemon
     db.execute(sql, name, type)
   end
   
-  def self.find(id_num, db)
-    pokemon = db.execute("SELECT * FROM pokemon where id = ?", id_num)
-    pokemon1 = self.new(pokemon)
-    pokemon1.id = pokemon[0][0]
-    pokemon1.name = pokemon[0][1]
-    pokemon1.type = pokemon[0][2]
-    pokemon1.db = db
-    pokemon1
-  end
+  # def self.find(id_num, db)
+  #   pokemon = db.execute("SELECT * FROM pokemon where id = ?", id_num)
+  #   pokemon1 = self.new(pokemon)
+  #   pokemon1.id = pokemon[0][0]
+  #   pokemon1.name = pokemon[0][1]
+  #   pokemon1.type = pokemon[0][2]
+  #   pokemon1.db = db
+  #   pokemon1
+  # end
   
+    def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+  end
 end
